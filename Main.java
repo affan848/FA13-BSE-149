a962098b17a949d8b2913e75b5b4d608da25b04c
affan848 <affan848@gmail.com>
2016-12-19 19:47:39 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package pack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		FA13007 shaheer = new FA13007();
+		shaheer.inMethod();
+		
+		FA13063 ayesha = new FA13063();
+		ayesha.inMethod();
+		
+		System.out.println("In Main FA13-BSE-149");
+		
+
+	}
+
+}
